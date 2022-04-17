2abb90b92096a87d8f60c7b6fdb80a84478b5643
Juan Ibiapina <juanibiapina@gmail.com>
2022-04-18 00:58:26 +0200

    chore: Configure domain for telegram webhooks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,4 +65,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Domain configuration so telegram-bot is able to configure the webhook
+  routes.default_url_options = { host: "radiant-badlands-16719.herokuapp.com", protocol: "https" }
 end
