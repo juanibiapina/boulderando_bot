c5b3c8efe47b621d48586be629af5b5a815baa7d
Juan Ibiapina <juanibiapina@gmail.com>
2022-04-29 01:14:23 +0200

    feat: Add uniqueness validation on users email

---
+++ db/migrate/20220428231243_add_index_to_users_email.rb
@@ -0,0 +1,5 @@
+class AddIndexToUsersEmail < ActiveRecord::Migration[7.0]
+  def change
+    add_index :users, :email
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_28_224716) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_28_231243) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_28_224716) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "birthday"
+    t.index ["email"], name: "index_users_on_email"
     t.index ["telegram_id"], name: "index_users_on_telegram_id"
   end
 
