509172fe487694de75c1bc6e4b4fa6697eeb49b6
Juan Ibiapina <juanibiapina@gmail.com>
2022-08-19 18:48:45 +0200

    Disable active record

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,7 +34,7 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
-  # config.use_active_record = false
+  config.use_active_record = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
